a14d64634d23a4e00d1f0e08e13a7c38c10ba906
Nikolay Stepin <bukato66@gmail.com>
2019-05-14 22:17:03 +0800

    added files

---
+++ precache-manifest.baa843f27b6c7d3f806cdcf5a42c7c40.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb490c02d40ce7c9c129caa7c39199ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ff549096e523b913a80",
+    "url": "/static/css/main.a55a0f79.chunk.css"
+  },
+  {
+    "revision": "d0a6111608b484a71278",
+    "url": "/static/js/2.3517570e.chunk.js"
+  },
+  {
+    "revision": "2ff549096e523b913a80",
+    "url": "/static/js/main.31487d88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d6e36b2f4ec13a27d96de469a52834b",
+    "url": "/static/media/alcor-bg.9d6e36b2.jpg"
+  },
+  {
+    "revision": "82db559f296fec74b9f6b14cdee36407",
+    "url": "/static/media/alcor.82db559f.svg"
+  },
+  {
+    "revision": "269941e4cf67086fb06bc4105b2d7ccf",
+    "url": "/static/media/dobrota-bg.269941e4.jpg"
+  },
+  {
+    "revision": "4693ed34d397cc75b0c76b801d1dd04a",
+    "url": "/static/media/dobrota.4693ed34.svg"
+  },
+  {
+    "revision": "e49a7fc665ff0f7afde1f0bc54161732",
+    "url": "/static/media/github.e49a7fc6.svg"
+  },
+  {
+    "revision": "09d6ecb584f4553ae88584c78f70373d",
+    "url": "/static/media/photo.09d6ecb5.jpg"
+  },
+  {
+    "revision": "d7b2e838690c74bee47c7d8ee58922b9",
+    "url": "/static/media/telegram.d7b2e838.svg"
+  },
+  {
+    "revision": "021d8859cd66328094d8f6b67779fafc",
+    "url": "/static/media/vk.021d8859.svg"
+  }
+]);
